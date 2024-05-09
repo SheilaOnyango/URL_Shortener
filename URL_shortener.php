31529daa0ba2e83a94b3e38f9e7731b76beb5e7d
sheilaonyango <edusheila31@gmail.com>
2024-05-09 12:10:37 +0300

    Access token and collect long url from form input

--- URL_shortener.php
+++ URL_shortener.php
@@ -6,4 +6,11 @@ include 'DBConnect.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // URL to the Unelma.IO API
     $url = 'https://unelma.io/api/v1/link';
+
+    //Access token for the Unelma.IO API
+    $accessToken = '27|ht0AeOmils4W1NtNtTs3bLmJ45mpJp9LVxuzguSe9ae0dd33';
+    
+    // Collect the long URL from the form input
+    $longUrl = $_POST['longUrl'];
+    
 }
