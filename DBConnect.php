28fb9ae88425f9d5f14244adf67ae1e81d438df6
sheilaonyango <edusheila31@gmail.com>
2024-05-09 12:38:13 +0300

    Database connection using PDO, establish database connection

--- DBConnect.php
+++ DBConnect.php
@@ -1,2 +1,30 @@
 <?php
 
+/* Database connection using PDO
+*/
+
+class DBConnect
+{
+    private $host = 'db'; 
+    private $dbname = 'URL_shortener';
+    private $user = 'root';
+    private $pass = 'lionPass';
+
+    public function connect()
+    {
+        try {
+            //Establish database connection
+            $conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->user, $this->pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (\PDOException $e) {
+            // Display detailed error message
+            echo "Database Error: " . $e->getMessage();
+            // Stop script execution
+            die();
+        }
+    }
+}
+
+?>
+
