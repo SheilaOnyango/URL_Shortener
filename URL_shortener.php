b8ecc60d9035e218fa70ba1c32ef56bc9f72e883
sheilaonyango <edusheila31@gmail.com>
2024-05-09 12:07:04 +0300

    Check if the form was submitted

--- URL_shortener.php
+++ URL_shortener.php
@@ -2,3 +2,8 @@
 
 include 'DBConnect.php';
 
+//Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // URL to the Unelma.IO API
+    $url = 'https://unelma.io/api/v1/link';
+}
